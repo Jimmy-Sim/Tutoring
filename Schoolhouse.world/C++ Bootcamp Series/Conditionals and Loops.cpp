ab909cdc1dbca59fd77c7b3f9470e7614aa08f5e
Jimmy-Sim <77947780+Jimmy-Sim@users.noreply.github.com>
2023-07-19 13:19:28 +0900

    Create Conditionals and Loops.cpp

---
+++ Schoolhouse.world/C++ Bootcamp Series/Conditionals and Loops.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  int number;
+  cin >> number;
+  // Conditions:
+  // Equal to:
+  ==
+          // Greater than:
+          >
+              // Less than:
+              <
+              // Greater than or equal to:
+              >=
+              // Less than or equal to:
+              <=
+              // Multiple conditions:
+              // And:
+              && or
+      and
+      // Or:
+      || or or
+
+      // If:
+      if (condition) {
+    action;
+  }
+  // Else if:
+  else if (condition) {
+    action;
+  }
+  // Else:
+  else {
+    action;
+  }
+
+  // Loops:
+  // For loop:
+  for (int i = 0; i < quantity; i++) {
+    action;
+  }
+  // While loop:
+  while (condition) {
+    action;
+  }
+}
