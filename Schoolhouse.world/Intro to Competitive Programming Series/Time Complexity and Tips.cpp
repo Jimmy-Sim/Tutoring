6ab0818bae0493e5803d184684eb626f9595a54c
Jimmy-Sim <77947780+Jimmy-Sim@users.noreply.github.com>
2023-07-20 11:22:53 +0900

    Create Time Complexity and Tips.cpp

---
+++ Schoolhouse.world/Intro to Competitive Programming Series/Time Complexity and Tips.cpp
@@ -0,0 +1,32 @@
+/*
+Different time complexities in Big O:
+  - O(1)
+  - O(N)
+  - O(N^2)
+  - O(2^N)
+  - etc.
+*/
+
+
+// Examples of different time complexities:
+
+// O(N):
+for (0 ... N) {
+    ...
+}
+
+// O(N^2):
+for (0 ... N) {
+    for (0 ... N) {
+        ...
+    }
+}
+
+
+// A few tips for competitive programming:
+
+// Fast input and output:
+ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+
+// Include every library in C++:
+#include <bits/stdc++.h>
